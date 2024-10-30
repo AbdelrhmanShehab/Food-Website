6214f66e257d74c26ec920a9eebfa3782ed9919b
AbdelrhmanShehab <abdlrhman.shihab@gmail.com>
2024-10-30 16:31:37 +0300

    second commit

--- script.js
+++ script.js
@@ -1,105 +1,119 @@
-let rightArrow = document.querySelector(".right-arrow");
-let leftArrow = document.querySelector(".left-arrow");
-let photo = document.querySelector(".photo");
-let paragraph = document.querySelector(".paragraph");
-let name = document.querySelector(".name");
-let jobTitle = document.querySelector(".job-title");
-let surpriseBtn = document.querySelector(".surprise");
-let namesArr = [
-  "Abdelrhman Hossam",
-  "Youssef Hossam",
-  "Yahya Ehab",
-  "Peter Jones",
-];
-let photosReviewArr = [
-  "imgs//reviews2.jpeg",
-  "imgs//reviews3.webp",
-  "imgs//reviews4.jpg",
-  "imgs//review1.jpeg",
-  "imgs//reviews2.jpeg",
-  "imgs//reviews3.webp",
-  "imgs//reviews4.jpg",
-  "imgs//review1.jpeg",
-];
+document.addEventListener("DOMContentLoaded", function () {
+  let plusIcons = document.querySelectorAll(".plus-icon");
+  let ansTexts = document.querySelectorAll(".ans-text");
 
-let paragraphsArr = [
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga rerum perspiciatis nisi non molestiae tempora facere nostrum sit atque. Commodi numquam iusto sequi magnam ad esse nihil nemo quasi. Tempora.",
-  "just a random text showing the change between reveiws",
-  "Another random text but now i want to make it longer to show different between different texts",
-  "and that's another one which will be between both reviwes in length",
-];
-
-let jobs = ["Software Engineer", "UX designer", "Doctor", "Intern"];
-let i = 0;
-
-if (rightArrow.addEventListener("click", rightSlide)) {
-  i++;
-} else if (leftArrow.addEventListener("click", leftSlide)) {
-  i--;
-}
-
-function rightSlide() {
-  photo.src = photosReviewArr[i];
-  paragraph.textContent = paragraphsArr[i];
-  jobTitle.textContent = jobs[i];
-  name.textContent = namesArr[i];
-  ++i;
-  if (i == jobs.length) {
-    i = 0;
+  // Ensure the elements exist on the current page before attaching event listeners
+  if (plusIcons.length && ansTexts.length) {
+    plusIcons.forEach(function (plusIcon, index) {
+      plusIcon.addEventListener("click", function () {
+        if (ansTexts[index].classList.contains("show-question")) {
+          ansTexts[index].classList.remove("show-question");
+        } else {
+          ansTexts[index].classList.add("show-question");
+          ansTexts.forEach((ans, i) => {
+            if (i !== index) ans.classList.remove("show-question");
+          });
+        }
+      });
+    });
   }
-}
 
-function leftSlide() {
-  photo.src = photosReviewArr[i];
-  paragraph.textContent = paragraphsArr[i];
-  jobTitle.textContent = jobs[i];
-  name.textContent = namesArr[i];
-  if (i == 0) {
-    i = jobs.length;
+  let rightArrow = document.querySelector(".right-arrow");
+  let leftArrow = document.querySelector(".left-arrow");
+  let photo = document.querySelector(".photo");
+  let paragraph = document.querySelector(".paragraph");
+  let name = document.querySelector(".name");
+  let jobTitle = document.querySelector(".job-title");
+  let surpriseBtn = document.querySelector(".surprise");
+  let namesArr = [
+    "Abdelrhman Hossam",
+    "Youssef Hossam",
+    "Yahya Ehab",
+    "Peter Jones",
+  ];
+  let photosReviewArr = [
+    "imgs/reviews2.jpeg",
+    "imgs/reviews3.webp",
+    "imgs/reviews4.jpg",
+    "imgs/review1.jpeg",
+  ];
+  let paragraphsArr = [
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    "just a random text showing the change between reviews",
+    "Another random text but longer",
+    "and that's another one which is in-between length",
+  ];
+  let jobs = ["Software Engineer", "UX designer", "Doctor", "Intern"];
+  let i = 0;
+
+  if (rightArrow) rightArrow.addEventListener("click", rightSlide);
+  if (leftArrow) leftArrow.addEventListener("click", leftSlide);
+
+  function rightSlide() {
+    photo.src = photosReviewArr[i];
+    paragraph.textContent = paragraphsArr[i];
+    jobTitle.textContent = jobs[i];
+    name.textContent = namesArr[i];
+    ++i;
+    if (i == jobs.length) i = 0;
   }
-  --i;
-}
 
-surpriseBtn.addEventListener("click", surpriseMe);
-
-function surpriseMe() {
-  i = Math.floor(Math.random(jobs.length) * jobs.length);
-  console.log(Math.floor(Math.random(jobs.length) * jobs.length));
-  photo.src = photosReviewArr[i];
-  paragraph.textContent = paragraphsArr[i];
-  jobTitle.textContent = jobs[i];
-  name.textContent = namesArr[i];
-}
-
-let sidebarBurgerMenu = document.querySelector(".sidebar-burger-menu");
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector(".close-btn");
-function scaleMenu() {
-  sidebarBurgerMenu.style.transition = "all .8s ease-in-out";
-  sidebarBurgerMenu.style.transform = "scale(2.4)";
-  setTimeout(scaleReverse, 500);
-}
-function scaleReverse() {
-  sidebarBurgerMenu.style.transition = "all .8s ease-in-out";
-  sidebarBurgerMenu.style.transform = "scale(1)";
-  setTimeout(scaleMenu, 500);
-}
-setTimeout(scaleMenu, 500);
-
-sidebarBurgerMenu.addEventListener("click", sidebarMenu);
-
-function sidebarMenu() {
-  if (sidebar.classList.contains("show-sidebar")) {
-    sidebar.classList.remove("show-sidebar");
-  } else {
-    sidebar.classList.add("show-sidebar");
+  function leftSlide() {
+    photo.src = photosReviewArr[i];
+    paragraph.textContent = paragraphsArr[i];
+    jobTitle.textContent = jobs[i];
+    name.textContent = namesArr[i];
+    if (i == 0) i = jobs.length;
+    --i;
   }
-}
 
-closeBtn.addEventListener("click", closeSidebar);
+  if (surpriseBtn) {
+    surpriseBtn.addEventListener("click", function () {
+      i = Math.floor(Math.random() * jobs.length);
+      photo.src = photosReviewArr[i];
+      paragraph.textContent = paragraphsArr[i];
+      jobTitle.textContent = jobs[i];
+      name.textContent = namesArr[i];
+    });
+  }
 
-function closeSidebar() {
-  sidebar.classList.remove("show-sidebar");
-}
+  let sidebarBurgerMenu = document.querySelector(".sidebar-burger-menu");
+  let sidebar = document.querySelector(".sidebar");
+  let closeBtn = document.querySelector(".close-btn");
 
-// javascript for second page QA section
+  if (sidebarBurgerMenu && sidebar && closeBtn) {
+    sidebarBurgerMenu.addEventListener("click", sidebarMenu);
+    closeBtn.addEventListener("click", closeSidebar);
+
+    function sidebarMenu() {
+      sidebar.classList.toggle("show-sidebar");
+    }
+
+    function closeSidebar() {
+      sidebar.classList.remove("show-sidebar");
+    }
+  }
+});
+
+//  filtering menu based on category
+const filterButtons = document.querySelectorAll(".food-flex button");
+const foodItems = document.querySelectorAll(".overlay-food");
+
+// end of container section
+filterButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const category = e.target.className;
+
+    foodItems.forEach((food) => {
+      if (category === "all") {
+        food.style.display = "block";
+      } else {
+        if (food.getAttribute("data-category") === category) {
+          food.style.display = "block";
+        } else {
+          food.style.display = "none";
+        }
+      }
+    });
+  });
+});
